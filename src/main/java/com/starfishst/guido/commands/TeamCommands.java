50f3fbbd025ce4c3ab90761690e9a9c136f5fea2
xChevy <chevyself@gmail.com>
2020-09-07 17:34:28 -0500

    Update team commands

--- src/main/java/com/starfishst/guido/commands/TeamCommands.java
+++ src/main/java/com/starfishst/guido/commands/TeamCommands.java
@@ -12,6 +12,7 @@ import java.util.List;
 import com.starfishst.core.utils.maps.Maps;
 import com.starfishst.guido.Guido;
 import com.starfishst.guido.lang.GuidoLanguageHandler;
+import com.starfishst.guido.util.Mentions;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.VoiceChannel;
@@ -24,6 +25,14 @@ public class TeamCommands {
   @NotNull
   private final GuidoLanguageHandler handler = Guido.getLanguageHandler();
 
+  /**
+   * Get two event teams from a voice channel. This will get the channel where the sender executed
+   *  the command, then, check if the number of people inside of it is event, if it is, this will return two
+   *  teams with an even number of players
+   * @param context the context the command
+   * @param sender the sender of the command
+   * @return the result of the command execution
+   */
   @Command(aliases = "teams", description = "cmd.teams.desc")
   public Result teams(CommandContext context, Member sender) {
     GuildVoiceState voiceState = sender.getVoiceState();
@@ -33,23 +42,12 @@ public class TeamCommands {
       if (members.size() % 2 == 0) {
         List<Member> team1 = RandomUtils.getRandom(members, members.size() / 2);
         List<Member> team2 = new ArrayList<>();
-        members.forEach(
-            member -> {
-              if (!team1.contains(member)) {
-                team2.add(member);
+          for (Member member : members) {
+              if (!team1.contains(member)){
+                  team2.add(member);
               }
-            });
-        List<String> tags1 = new ArrayList<>();
-        team1.forEach(
-            member -> {
-              tags1.add(member.getAsMention());
-            });
-        List<String> tags2 = new ArrayList<>();
-        team2.forEach(
-            member -> {
-              tags2.add(member.getAsMention());
-            });
-        return new Result(handler.getFile(context).get("cmd.teams.result", Maps.builder("team1", Lots.pretty(tags1)).append("team2", Lots.pretty(tags2))));
+          }
+        return new Result(handler.getFile(context).get("cmd.teams.result", Maps.builder("team1", Mentions.pretty(team1)).append("team2", Mentions.pretty(team2))));
       } else {
         return new Result(handler.getFile(context).get("cmd.teams.not-even"));
       }
@@ -58,29 +56,34 @@ public class TeamCommands {
     }
   }
 
+  /**
+   * This will get the number given of captains. From the text channel where the sender executed the
+   * command it will return the number given of random people, obviously, if the number given is higher than the people in the
+   * channel it will not be completed
+   *
+   * @param context the context of the command
+   * @param sender the sender of the command
+   * @param number the number of leaders to get
+   * @return the result of the command
+   */
   @Command(aliases = {"leaders", "capitanes"}, description = "cmd.leaders.desc")
   public Result capitanes(
           CommandContext context,
       Member sender,
       @Optional(name = "number", description = "number", suggestions = "2")
-          int numb) {
+          int number) {
     GuildVoiceState voiceState = sender.getVoiceState();
     if (voiceState != null && voiceState.getChannel() != null) {
       VoiceChannel channel = voiceState.getChannel();
       List<Member> members = channel.getMembers();
-      if (members.size() >= numb) {
-        List<Member> random = RandomUtils.getRandom(members, numb);
-        List<String> tags = new ArrayList<>();
-        random.forEach(
-            member -> {
-              tags.add(member.getAsMention());
-            });
-        return new Result(handler.getFile(context).get("cmd.leaders.result",  Maps.singleton("leaders", Lots.pretty(tags))));
+      if (members.size() >= number) {
+        List<Member> random = RandomUtils.getRandom(members, number);
+        return new Result(handler.getFile(context).get("cmd.leaders.result",  Maps.singleton("leaders", Mentions.pretty(random))));
       } else {
-        return new Result(handler.getFile(context).get("cmd.leaders.not-enough",  Maps.singleton("number", String.valueOf(numb))));
+        return new Result(handler.getFile(context).get("cmd.leaders.not-enough",  Maps.singleton("number", String.valueOf(number))));
       }
     } else {
-      return new Result(handler.getFile(context).get("cmd.leaders.not-connected",  Maps.singleton("number", String.valueOf(numb))));
+      return new Result(handler.getFile(context).get("cmd.leaders.not-connected",  Maps.singleton("number", String.valueOf(number))));
     }
   }
 }
